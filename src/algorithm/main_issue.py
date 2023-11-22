76012510251211f2d94ffb80ab9d8cfb37c58877
Guido Annicchiarico <g.annicchiarico@studenti.unisannio.it>
2023-11-22 14:22:31 +0100

    Refactoring, code restructuring. I used the CHATGPT code to create functions dedicated to a specific task. . Now data extraction and timestamp comparison is done in a dedicated function. I also created a function that collects the operations initially called by both szz and szz_issue (operations).

--- src/algorithm/main_issue.py
+++ src/algorithm/main_issue.py
@@ -6,13 +6,12 @@ import re
 
 
 # %%
-def get_diff(repo_path, commit_A, commit_B):
-    repo = git.Repo(repo_path)
+def get_diff(repo, commit_A, commit_B):
     diff = repo.git.diff(commit_A.hexsha, commit_B.hexsha, '-U0', '--histogram')
     return diff
 
 
-def get_bug_fix_commits(repo):
+def get_bug_fix_commits_szz_issue(repo):
     commits = repo.iter_commits()
     # retrieve bug fix commit
     bug_fix_commits = []
@@ -48,7 +47,9 @@ def generate_changes_dict(diff_output):
             start_line = int(line_number_match.group(1))
             num_lines = 1 if line_number_match.group(3) is None else int(line_number_match.group(3))
 
-            numbers_list.extend(range(start_line, start_line + num_lines))
+            # Aggiungi le linee modificate solo se non sono commenti
+            if not match_comment(line):
+                numbers_list.extend(range(start_line, start_line + num_lines))
 
     if current_file_path and numbers_list:
         result_dict[current_file_path] = numbers_list
@@ -106,53 +107,86 @@ def print_candidate_commit(total_candidate_commits):
         for com in value:
             print(com)
 
-
-def szz(path_to_repo):
-    repo = git.Repo(path_to_repo)
+def get_bug_fix_commits_for_szz(repo):
     commits = repo.iter_commits()
-    # retrieve bug fix commit
     bug_fix_commits = []
     for commit in commits:
         commit_message = commit.message.lower()
-        if 'bug' in commit_message and ('fix' or 'fixed') in commit_message:
+        if 'bug' in commit_message and ('fix' in commit_message or 'fixed' in commit_message):
             bug_fix_commits.append(commit)
+
+    return bug_fix_commits
+
+def operations(repo, bug_fix_commit):
+    all_candidate_commits =[]
+    # verifichiamo se il commit ha effettivamente un parent da confrontare, altrimenti non possiamo fare il
+    # confronto
+    if bug_fix_commit.parents is not None:
+        parent_commit = bug_fix_commit.parents[0];
+        diff = get_diff(repo, bug_fix_commit, parent_commit)
+
+        # generiamo il dizionario che contiene come chiave i file cambiati e come valore i numeri di riga
+        # modificati, ed in particolare le linee che dal commit parent sono state eliminate e sostituite col fix
+        # del bug
+        changes_dict = generate_changes_dict(diff)
+        # una volta fatto ciò la funzione all_candidate_commits trova i commit che hanno modificato quelle linee
+        # l'ultima volta
+        all_candidate_commits = get_all_candidate_commits(repo, parent_commit, changes_dict)
+
+    return all_candidate_commits
+
+
+def szz(repo):
+    bug_fix_commits = get_bug_fix_commits_for_szz(repo)
 
     total_candidate_commit = {}
     # iteriamo su tutti i commit bug_fix
     for bug_fix_commit in bug_fix_commits[0:5]:
-        # verifichiamo se il commit ha effettivamente un parent da confrontare, altrimenti non possiamo fare il
-        # confronto
-        if bug_fix_commit.parents is not None:
-            # se non è nullo facciamo il diff
-            parent_commit = bug_fix_commit.parents[0];
-            diff = get_diff(path_to_repo, bug_fix_commit, parent_commit)
-
-            # generiamo il dizionario che contiene come chiave i file cambiati e come valore i numeri di riga
-            # modificati, ed in particolare le linee che dal commit parent sono state eliminate e sostituite col fix
-            # del bug
-            changes_dict = generate_changes_dict(diff)
-            # una volta fatto ciò la funzione all_candidate_commits trova i commit che hanno modificato quelle linee
-            # l'ultima volta
-            all_candidate_commits = get_all_candidate_commits(repo, parent_commit, changes_dict)
-            total_candidate_commit[bug_fix_commit] = all_candidate_commits
+        # chiamiamo la funzione che fa diff, blame e ottiene i commit candidati
+        total_candidate_commit[bug_fix_commit] = operations(repo, bug_fix_commit)
 
     print_candidate_commit(total_candidate_commit)
 
+def extract_issue_number(commit_message):
+    pattern = re.compile(r'#(\d+)')
+    match = pattern.search(commit_message)
+    if match:
+        return int(match.group(1))
+    return None
 
-def szz_issue(path_to_repo, issue_data):
+def extract_and_compare_timestamp(all_candidate_commits, issue_opened_at):
+    suspect_commit = []
+
+    # Itera su ciascun commit candidato ad essere commit che ha introdotto il bug ottenuto dal blame
+    for commit_sha, author in all_candidate_commits:
+        # per ogni commit candidato, estraiamo la data
+        commit_bug = repo.commit(commit_sha)
+        # Ottieni la data del commit come timestamp
+        commit_date_timestamp = commit_bug.committed_date
+
+        # Converti la stringa ISO 8601 in un oggetto datetime
+        issue_opened_at_datetime = datetime.fromisoformat(issue_opened_at.replace('Z', '+00:00'))
+
+        # Estrai il timestamp Unix
+        timestamp_issue_opened_at = int(issue_opened_at_datetime.timestamp())
+
+        # Stampa solo i commit effettuati prima della data di apertura dell'issue
+        # cioè che sicuramente non sono fix parziali
+        if commit_date_timestamp < timestamp_issue_opened_at:
+            suspect_commit.append((commit_sha, commit_bug.author.name))
+
+    return suspect_commit
+
+def szz_issue(repo, issue_data):
     suspect_commit_dict = {}
-    repo = git.Repo(path_to_repo)
 
-    bug_fix_commits = get_bug_fix_commits(repo)
-    # supponiamo che le pull request contenute nel file siano già "closed"
+    bug_fix_commits = get_bug_fix_commits_szz_issue(repo)
     for bug_fix_commit in bug_fix_commits:
 
-        pattern = re.compile(r'#(\d+)')
-        match = pattern.search(bug_fix_commit.message)
-        issue_number_in_bug_fix = int(match.group(1))
+        issue_number_in_bug_fix = extract_issue_number(bug_fix_commit.message)
         # supponiamo che l'sha del commit bug_fix sia l'ultimo della pull request, quello che ne indica la chiusura
         commit_sha_bug_fix = bug_fix_commit.hexsha
-        issue_opened_at = None
+
         print(f'The bug fix commit: {commit_sha_bug_fix} refers to issue {issue_number_in_bug_fix}')
         found = False
         for issue in issue_data:
@@ -166,43 +200,16 @@ def szz_issue(path_to_repo, issue_data):
                       f"for commits")
                 issue_opened_at = issue['created_at']
 
-                # andiamo a prendere il commit bug_fix avendo l'sha e il primo parent commit
-                parent_commit = bug_fix_commit.parents[0]
+                # chiamiamo la funzione che fa diff, blame e ottiene i commit candidati
+                all_candidate_commits = operations(repo, bug_fix_commit)
 
-                # facciamo il diff tra i due per ottenere le linee di codice cambiate
-                diff = get_diff(path_to_repo, bug_fix_commit, parent_commit)
-                changes_dic = generate_changes_dict(diff)
-
-                # effettuiamo blame su tutte le modifiche per ottenere i commit che hanno introdotto le linee
-                # modificate
-                all_candidate_commits = get_all_candidate_commits(repo, parent_commit, changes_dic)
-                suspect_commit = []
-
-                # Itera su ciascun commit candidato ad essere commit che ha introdotto il bug ottenuto dal blame
-                for commit_sha, author in all_candidate_commits:
-                    # per ogni commit candidato, estraiamo la data
-                    commit_bug = repo.commit(commit_sha)
-                    # Ottieni la data del commit come timestamp
-                    commit_date_timestamp = commit_bug.committed_date
-
-                    # Converti la stringa ISO 8601 in un oggetto datetime
-                    issue_opened_at_datetime = datetime.fromisoformat(issue_opened_at.replace('Z', '+00:00'))
-
-                    # Estrai il timestamp Unix
-                    timestamp_issue_opened_at = int(issue_opened_at_datetime.timestamp())
-
-                    # Stampa solo i commit effettuati prima della data di apertura dell'issue
-                    # cioè che sicuramente non sono fix parziali
-                    if commit_date_timestamp < timestamp_issue_opened_at:
-                        suspect_commit.append((commit_sha, commit_bug.author.name))
-                suspect_commit_dict[commit_sha_bug_fix] = suspect_commit
+                suspect_commit_dict[commit_sha_bug_fix] = extract_and_compare_timestamp(all_candidate_commits, issue_opened_at)
         if not found:
-            print(
-                f'The bug_fix_commit: {commit_sha_bug_fix} contains a reference to issue {issue_number_in_bug_fix} but'
+            print(f'The bug_fix_commit: {commit_sha_bug_fix} contains a reference to issue {issue_number_in_bug_fix} but'
                 f'is not contained in the file that has been passed')
 
     print('\n\n\nThis is the list of every bug fix commits and the relative bug inducing commits')
-    print(suspect_commit_dict)
+    print_candidate_commit(suspect_commit_dict)
 
 
 if __name__ == '__main__':
@@ -217,14 +224,16 @@ if __name__ == '__main__':
     args = parser.parse_args()
     path_to_repo = args.repo_path
 
+    repo = git.Repo(path_to_repo)
+
     if args.issue:
         # Se -i è presente, richiama la funzione szz_issue con il parametro --issue
         try:
             with open(args.issue) as issue_path_file:
                 issue_data = json.load(issue_path_file)
-            szz_issue(path_to_repo, issue_data)
+            szz_issue(repo, issue_data)
         except json.JSONDecodeError as e:
             print(f"Error decoding JSON content: {e}")
     else:
         # Altrimenti, richiama la funzione alternativa senza il parametro --issue
-        szz(path_to_repo)
+        szz(repo)
